2832ca5aa19be0bab569a5b0d56d5ce16320b45f
ahliko <killian.guillemot@ynov.com>
2024-01-11 12:22:29 +0100

    Update Files

--- init.py
+++ init.py
@@ -1,12 +1,12 @@
 import os
 
-if not os.path.exists('/var/log/monit.log'):
-    os.system('touch /var/log/monit.log')
-    os.system('chown $USER:$USER /var/log/monit.log')
-
 if not os.path.exists('/var/monit'):
     os.system('mkdir /var/monit')
     os.system('chown $USER:$USER /var/monit')
+
+if not os.path.exists('/var/log/monit.log'):
+    os.system('touch /var/log/monit.log')
+    os.system('chown $USER:$USER /var/log/monit.log')
 
 if not os.path.exists('/etc/monit'):
     os.system('touch /etc/monit')
