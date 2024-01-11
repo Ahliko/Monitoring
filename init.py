efaf1e53a1ec86c5f81516ec86212303f34fe0f1
ahliko <killian.guillemot@ynov.com>
2024-01-11 12:31:23 +0100

    Update Files

--- init.py
+++ init.py
@@ -1,17 +1,21 @@
 import os
+import pwd
+
+if pwd.getpwnam('monit') is None:
+    os.system('useradd monit --shell /sbin/nologin')
 
 if not os.path.exists('/var/monit'):
     os.system('mkdir /var/monit')
-    os.system('chown $USER:$USER /var/monit')
+    os.system('chown monit:monit /var/monit')
 
 if not os.path.exists('/var/log/monit.log'):
     os.system('touch /var/log/monit.log')
-    os.system('chown $USER:$USER /var/log/monit.log')
+    os.system('chown monit:monit /var/log/monit.log')
 
 if not os.path.exists('/etc/monit'):
     os.system('mkdir /etc/monit')
-    os.system('chown $USER:$USER /etc/monit')
+    os.system('chown monit:monit /etc/monit')
 
 if not os.path.exists('/etc/monit/monit.json'):
     os.system('touch /etc/monit/monit.json')
-    os.system('chown $USER:$USER /etc/monit/monit.json')
+    os.system('chown monit:monit /etc/monit/monit.json')
