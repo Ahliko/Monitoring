1c4ff66aab0695f1a9974e2c264cda1dbb98d2f5
ahliko <killian.guillemot@ynov.com>
2024-01-12 12:54:48 +0100

    Update Files

--- monit.py
+++ monit.py
@@ -69,7 +69,7 @@ class Monitoring:
         if not os.path.exists(f'{self.__path}/monit_conf.json'):
             os.system(f'touch {self.__path}/monit_conf.json')
         with open(f"{self.__path}/monit_conf.json", "r") as f:
-            data = json.load(f)
+            data = json.loads(f.read())
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         for port in data["CHECK_PORTS"]:
             port_status = sock.connect_ex(("127.0.0.1", port)) == 0
