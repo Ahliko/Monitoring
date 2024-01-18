23895a5157bb6b45c50a426d1e8a08bf16bba524
ahliko <killian.guillemot@ynov.com>
2024-01-18 10:41:13 +0100

    Update Files

--- monit.py
+++ monit.py
@@ -109,6 +109,7 @@ class Monitoring:
         disk = []
         for file in file_list:
             with open(file, "r") as f:
+                self.__logger.debug(f"Reading file: {file}")
                 for line in f.readlines():
                     if line.startswith("cpu"):
                         cpu.append(float(line.split(":")[1].split("%")[0].strip()))
