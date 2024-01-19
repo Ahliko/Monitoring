3b4535f7b07eb941f77f7c284028006786d58314
ahliko <killian.guillemot@ynov.com>
2024-01-19 10:54:54 +0100

    Update Files

---
+++ client.py
@@ -0,0 +1,18 @@
+import grpc
+import monit_pb2
+import monit_pb2_grpc
+from google.protobuf import empty_pb2
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = monit_pb2_grpc.MonitServiceStub(channel)
+
+    # Call the method Check
+    stub.Check(empty_pb2.Empty())
+
+    # Call the method GetLast
+    response = stub.GetLast(empty_pb2.Empty())
+    print("Result from GetLast: ", response.result_json)
+
+if __name__ == '__main__':
+    run()
