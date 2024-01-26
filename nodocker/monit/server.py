a5f8edc3f6af9ca3ea20109810751a0291a6a31f
ahliko <killian.guillemot@ynov.com>
2024-01-26 12:01:24 +0100

    Update docker

--- nodocker/monit/server.py
+++ nodocker/monit/server.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python
-import grpc
 from concurrent import futures
-import monit_pb2, monit_pb2_grpc
+
+import grpc
 from google.protobuf import empty_pb2
+
+import monit_pb2
+import monit_pb2_grpc
 from monit import Monitoring
 
 
@@ -27,11 +30,11 @@ class MonitServiceServicer(monit_pb2_grpc.MonitServiceServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     monit_pb2_grpc.add_MonitServiceServicer_to_server(MonitServiceServicer(), server)
-    server.add_insecure_port('[::]:50051')
-    print('Starting server on port 50051...')
+    server.add_insecure_port("[::]:50051")
+    print("Starting server on port 50051...")
     server.start()
     server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     serve()
